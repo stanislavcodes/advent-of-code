dafeefb87eeb8b67597992eb4c154038f7bf6725
Stanislav Korchevskyi <stanislav.codes@gmail.com>
2024-12-07 10:19:21 +0200

    solved day seven part one

---
+++ day7/a.ts
@@ -0,0 +1,72 @@
+const path = 'day7/input.txt';
+const file = Bun.file(path);
+const input = await file.text();
+
+const operators = ['+', '*'];
+
+function generateOperatorVariants(
+  symbols: string[],
+  length: number
+): string[][] {
+  const results: string[][] = [];
+
+  function backtrack(current: string[]): void {
+    if (current.length === length) {
+      results.push([...current]);
+      return;
+    }
+
+    for (const symbol of symbols) {
+      current.push(symbol);
+      backtrack(current);
+      current.pop();
+    }
+  }
+
+  backtrack([]);
+  return results;
+}
+
+const equations = input.split('\n').map((line) => {
+  const [res, rest] = line.split(': ');
+  const result = parseInt(res, 10);
+  const numbers = rest.split(' ').map((n) => parseInt(n, 10));
+
+  return {
+    result,
+    numbers,
+  };
+});
+
+let totalCalibrationResult = 0;
+
+for (const equation of equations) {
+  const { result, numbers } = equation;
+
+  const operatorVariants = generateOperatorVariants(
+    operators,
+    numbers.length - 1
+  );
+
+  for (const variant of operatorVariants) {
+    let current = numbers[0];
+
+    for (let i = 0; i < variant.length; i++) {
+      const operator = variant[i];
+      const number = numbers[i + 1];
+
+      if (operator === '+') {
+        current += number;
+      } else if (operator === '*') {
+        current *= number;
+      }
+    }
+
+    if (current === result) {
+      totalCalibrationResult += result;
+      break;
+    }
+  }
+}
+
+console.log('What is their total calibration result?', totalCalibrationResult);
